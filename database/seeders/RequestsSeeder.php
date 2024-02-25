1009003790aa93435490faf4ec61306166cc616c
GabrielR4SH <bieelsantana23@hotmail.com>
2024-02-25 20:34:59 -0300

    Seeder Resolver

--- database/seeders/RequestsSeeder.php
+++ database/seeders/RequestsSeeder.php
@@ -11,28 +11,27 @@ class RequestsSeeder extends Seeder
     public function run()
     {
         Redirect::create([
-            'url_destino' => 'https://www.google.com.br',
+            'url_destino' => 'https://www.google.com',
             'code' => Hashids::encode(1), // Gerando um código para o primeiro registro
             'ativo' => true,
         ]);
 
         Redirect::create([
-            'url_destino' => 'https://www.example.com',
+            'url_destino' => 'https://www.youtube.com',
             'code' => Hashids::encode(2),
             'ativo' => true,
         ]);
 
         Redirect::create([
-            'url_destino' => 'https://www.yahoo.com',
+            'url_destino' => 'https://www.bing.com',
             'code' => Hashids::encode(3),
             'ativo' => false,
         ]);
 
         Redirect::create([
-            'url_destino' => 'https://www.microsoft.com',
+            'url_destino' => 'https://www.twitter.com',
             'code' => Hashids::encode(4),
             'ativo' => false,
         ]);
     }
-
 }
